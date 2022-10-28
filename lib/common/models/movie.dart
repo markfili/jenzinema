167a7337820b4eb096ce555e27195eeed4409c1f
Marko Filipovic <mrkfilipovic3@gmail.com>
2022-10-28 20:28:24 +0200

    make json use snake field rename function, initialize genres field

--- lib/common/models/movie.dart
+++ lib/common/models/movie.dart
@@ -4,32 +4,25 @@ import 'genre.dart';
 
 part 'movie.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(fieldRename: FieldRename.snake)
 class Movie {
   final bool? adult;
-  @JsonKey(name: "backdrop_path")
   final String? backdropPath;
-  @JsonKey(name: "genre_ids")
   final List<int>? genreIds;
-  @JsonKey(ignore: true)
-  late final List<Genre> genres;
   final int? id;
-  @JsonKey(name: "original_language")
   final String? originalLanguage;
-  @JsonKey(name: "original_title")
   final String? originalTitle;
   final String? overview;
   final double? popularity;
-  @JsonKey(name: "poster_path")
   final String? posterPath;
-  @JsonKey(name: "release_date")
   final String? releaseDate;
   final String? title;
   final bool? video;
-  @JsonKey(name: "vote_average")
   final double? voteAverage;
-  @JsonKey(name: "vote_count")
   final int? voteCount;
+
+  @JsonKey(ignore: true)
+  final List<Genre> genres = [];
 
   Movie({
     this.adult,
