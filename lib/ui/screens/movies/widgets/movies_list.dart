b2bbc097d37b51bd0f6995e8480ed5a31b99ee09
Marko Filipovic <mrkfilipovic3@gmail.com>
2022-11-03 14:54:48 +0100

    add footer item to movies list

--- lib/ui/screens/movies/widgets/movies_list.dart
+++ lib/ui/screens/movies/widgets/movies_list.dart
@@ -3,50 +3,44 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../../common/models/movie.dart';
 import '../../../common/helpers/insets.dart';
-import '../presenters/movies_presenter.dart';
 import 'movie_item.dart';
 
 class MoviesList extends ConsumerWidget {
   final List<Movie> movies;
   final ScrollController? scrollController;
+  final Widget? footer;
 
   const MoviesList(
     this.movies, {
     this.scrollController,
+    this.footer,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Stack(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Scrollbar(
-                child: ListView.separated(
-                  controller: scrollController,
-                  itemCount: movies.length,
-                  padding: const EdgeInsets.all(Insets.large + Insets.small),
-                  itemBuilder: (_, index) => MovieItem(movies[index]),
-                  separatorBuilder: (_, __) => SizedBox.fromSize(size: const Size.fromHeight(20)),
-                ),
-              ),
+        Expanded(
+          child: Scrollbar(
+            child: ListView.separated(
+              controller: scrollController,
+              itemCount: movies.length + ((footer == null) ? 0 : 1),
+              padding: const EdgeInsets.all(Insets.large + Insets.small),
+              itemBuilder: _buildItem,
+              separatorBuilder: (_, __) => SizedBox.fromSize(size: const Size.fromHeight(20)),
             ),
-          ],
+          ),
         ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: ref.watch(moviesPresenter).state.maybeWhen(
-                loading: (movies) => const PreferredSize(
-                  preferredSize: Size.fromHeight(8),
-                  child: LinearProgressIndicator(),
-                ),
-                orElse: () => const SizedBox.shrink(),
-              ),
-        )
       ],
     );
+  }
+
+  Widget _buildItem(_, index) {
+    if (footer != null && index == movies.length) {
+      return ListTile(title: footer);
+    }
+    return MovieItem(movies[index]);
   }
 }
